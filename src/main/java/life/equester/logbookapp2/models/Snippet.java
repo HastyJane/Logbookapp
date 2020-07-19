859833c45790986552142c9dc0d2f699cf167164
Hayley Smith <hayley.j.smith@gmail.com>
2020-07-19 09:17:24 -0500

    4th commit

--- src/main/java/life/equester/logbookapp2/models/Snippet.java
+++ src/main/java/life/equester/logbookapp2/models/Snippet.java
@@ -1,6 +1,37 @@
 package life.equester.logbookapp2.models;
 
+
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Entity
 public class Snippet extends AbstractEntity {
 
+private String snippet;
+private LocalDateTime timeStamp;
 
+    public Snippet() {
+
+    }
+
+    public Snippet(String snippet, LocalDateTime timeStamp) {
+        this.snippet = snippet;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getSnippet() {
+        return snippet;
+    }
+
+    public void setSnippet(String snippet) {
+        this.snippet = snippet;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
--- src/main/java/life/equester/logbookapp2/models/data/SnippetRepository.java
+++ src/main/java/life/equester/logbookapp2/models/data/SnippetRepository.java
@@ -1,16 +1,12 @@
 package life.equester.logbookapp2.models.data;
 
-import life.equester.logbookapp2.models.User;
+import life.equester.logbookapp2.models.Snippet;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import javax.transaction.Transactional;
 
 
 @Repository
 @Transactional
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-    User findByUsername(String username);
-
+public interface SnippetRepository extends CrudRepository<Snippet, String> {
 }
